ef3a6f8b54edf4c7647a5273f7a8416666b5c955
nies-ch <65388719+nies-ch@users.noreply.github.com>
2022-08-25 09:45:36 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
-# Check_MK with SMS Notification using SMSEagle
-Plugin for Check_MK SMS alerts &amp; notifications with use of SMSEagle device
+# CheckMK with SMS Notification using SMSEagle
 
-The following Python script can be used for Check_MK to send SMS alerts using SMSEagle devices. A short message will only be sent, when the user has entered a valid phone number as pager address, according to PHONE_REGEX. Be careful when copying the Python code, because that language highly depends on how the block indentation with white space is done. Also syntax and library names change with different Python versions. It was my first Python script to learn that language.
+Notification plugin for Check_MK SMS alerts and notifications with use of SMSEagle device.
 
-Further details about Check_MK and SMSEagle:  
-- https://checkmk.com/cms_notifications.html
-- https://www.smseagle.eu/api/
+The following Python script can be used for CheckMK to send SMS alerts using SMSEagle devices. A short message will only be sent, when the user has entered a valid phone number as pager address, according to phone_regex. Be careful when copying the Python code, because that language highly depends on how the block indentation with white space is done. Also syntax and library names change with different Python versions. It was my first Python script to learn that language.
+
+## Sample Configuration File in JSON Format
+
+API supports user/password login and access token style. The SMSEagle devices are processed in the order defined. If sending fails, then send alert via mail using defined mail_hosts.
+
+```
+{
+  "eagle_hosts": [
+    { "api_send_sms": "http://1.2.3.4/http_api/send_sms", "login": "eagleuser", "pass": "********" },
+    { "api_send_sms": "http://1.2.3.5/http_api/send_sms", "access_token": "**********************" }
+  ],
+  "eagle_timeout": 10,
+  "phone_regex": "^(\\+|00)\\d+$",
+  "mail_from" : "checkmk@example.com",
+  "mail_to"   : "monitoring@example.com",
+  "mail_hosts": [
+    { "host": "localhost", "port": 25 },
+    { "host": "smtp1.example.com", "port": 25 },
+    { "host": "smtp2.example.com", "port": 25 }
+  ]
+}
+```
 
 ## Installation
 
@@ -20,8 +39,20 @@ omd reload
 
 ## Changes
 
+- 2022-08-25: Moved configuration parameters into dedicated JSON file.
 - 2020-05-15: Publish to github.com. Rewrote from Python 2 to Python 3.
 - 2018-09-05: Try multiple mail servers. Restrict mobile number to +41 or 0041.
 - 2018-08-28: Don't send if NOTIFY_CONTACTPAGER is invalid format. 
 - 2018-08-27: Added NOTIFY_NOTIFICATIONTYPE.
 - 2018-08-23: First release
+
+## To Do
+
+- Adjust code to get Get 3G/4G signal strenght and modem state via API before sending. If that's not okay, show error message and choose next SMSEagle device
+
+
+## References
+
+Further details about Check_MK and SMSEagle:  
+- https://checkmk.com/cms_notifications.html
+- https://www.smseagle.eu/api/
--- notify_smseagle.py
+++ notify_smseagle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+# =============================================================================
+# NOTIFY VIA SMS USING SMSEAGLE FAILOVER
+# =============================================================================
 
+import json
 import urllib.request
 import urllib.parse
 import smtplib
@@ -9,19 +13,28 @@ import socket
 from os import environ
 from email.mime.text import MIMEText
 
-### Configuration Parameters
 
-EAGLE_HOSTS    = [ '1.2.3.4', '5.6.7.8', '9.10.11.12' ]
-EAGLE_LOGIN    = 'eagleuser'
-EAGLE_PASSWORD = 'eaglepassword'
-EAGLE_TIMEOUT  = 10
-PHONE_REGEX    = '^(\+|00)41\d+$'
-MAIL_FROM      = 'check_mk@example.com'
-MAIL_TO        = 'my-email@example.com'
-MAIL_HOSTS     = [ 'localhost', 'smtp1.example.com', 'smtp2.example.com' ]
-MAIL_PORT      = 25
+# -----------------------------------------------------------------------------
+# Configuration Parameters in JSON file
+# -----------------------------------------------------------------------------
+CONFIG_FILE = environ['HOME'] + '/.notify_smseagle.json'
 
 
+# -----------------------------------------------------------------------------
+# read_jsonfile
+# -----------------------------------------------------------------------------
+def read_jsonfile(file):
+    data = []
+    with open(file) as json_file:
+        data = json.load(json_file)
+    # Defaults
+    data["eagle_timeout"] = 10 if not "eagle_timeout" in data else data["eagle_timeout"]
+    return data
+
+
+# ----------------------------------------------------------------------------
+# Complain 
+# ----------------------------------------------------------------------------
 def complain(error):
     """
     Complain about errors via email. If that fails, just print the error
@@ -29,29 +42,33 @@ def complain(error):
     """
     print(error)
     msg = MIMEText(error)
-    msg['To'] = MAIL_TO
-    msg['From'] = MAIL_FROM
+    msg['To'] = cfg['mail_to']
+    msg['From'] = cfg['mail_from'] 
     msg['Subject'] = 'Check_MK: SMS Notification Error'
-    message_sent  = False
+    MessageSent  = False
 
-    for host in MAIL_HOSTS:
-        if not message_sent:
+    for mail_host in cfg['mail_hosts']:
+        if not MessageSent:
             try:
-                server = smtplib.SMTP(host, MAIL_PORT)
-                server.sendmail(MAIL_FROM, MAIL_TO, msg.as_string())
+                server = smtplib.SMTP(mail_host['host'], mail_host['port'])
+                server.sendmail(cfg['mail_from'], cfg['mail_to'], msg.as_string())
                 server.quit()
             except socket.error as e:
-                print("Could not connect to " + host + ": " + str(e))
+                print("Could not connect to " + mail_host['host'] + ": " + str(e))
             except smtplib.SMTPException as e:
                 print("Sending email failed: " + str(e))
             except:
                 print("Unknown error:", sys.exc_info()[0])
             else:
-                message_sent = True
+                MessageSent = True
                 break
     return
 
 
+
+# ----------------------------------------------------------------------------
+# Format Message 
+# ----------------------------------------------------------------------------
 def format_message():
     """
     Format the SMS message using the environment variables given by Check_MK.
@@ -78,44 +95,60 @@ def format_message():
     return msg
 
 
-def send_eagle_sms(to,message):
+
+# ----------------------------------------------------------------------------
+# Send Eagle SMS
+# ----------------------------------------------------------------------------
+def send_eagle_sms(to, message):
     """
-    Sending SMS via SMSEagle HTTP API. Uses hosts defined in list EAGLE_HOSTS.
+    Sending SMS via SMSEagle HTTP API. Uses hosts defined in list cfg['eagle_hosts'].
     Upon failure an email with the error is sent and next host is used.
     """
-    query_args   = { 'login':EAGLE_LOGIN, 'pass':EAGLE_PASSWORD, 'to':to,
-                     'message':message }
-    encoded_args = urllib.parse.urlencode(query_args)
-    message_sent  = False
-
-    for host in EAGLE_HOSTS:
-        url =  'http://%s/index.php/http_api/send_sms?%s' % (host, encoded_args)
-        if not message_sent:
+    MessageSent  = False
+    ErrorMessage = ''
+    for eagle_host in cfg['eagle_hosts']:
+        query_args = {
+            'to':  to,
+            'message': message }
+        if 'access_token' in eagle_host:
+             query_args['access_token'] = eagle_host['access_token']
+        else:
+            query_args['login'] = eagle_host['login']
+            query_args['pass']  = eagle_host['pass']
+        encoded_args = urllib.parse.urlencode(query_args)
+        url = eagle_host['api_send_sms'] + '?' + encoded_args
+        if not MessageSent:
             try:
-                result = urllib.request.urlopen(url,None,EAGLE_TIMEOUT).read()
+                result = urllib.request.urlopen(url, None, cfg['eagle_timeout']).read()
             except urllib.request.HTTPError as e:
                 complain('Sending SMS via SMSEagle %s failed: %s' 
-                          % (host, str(e.code))) 
+                          % (eagle_host['api_send_sms'], str(e.code))) 
             except urllib.request.URLError as e:
                 complain('Sending SMS via SMSEagle %s failed: %s'
-                          % (host, str(e.args)))
+                          % (eagle_host['api_send_sms'], str(e.args)))
             else:    
-                if result.startswith('OK;'):
-                    message_sent = True
-                    print('SMS successfully sent via %s to %s.' % (host, to))
+                if result.startswith(b'OK;'):
+                    MessageSent = True
+                    print('SMS successfully sent via %s to %s.' % (eagle_host['api_send_sms'], to))
                     break
                 else:
-                    message_sent = False
+                    MessageSent = False
                     complain('Sending SMS via SMSEagle %s failed: %s' 
-                             % (host, result.rstrip()))
-            
+                             % (eagle_host['api_send_sms'], result.rstrip()))
     return
 
+
+
+# ----------------------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------------------
+
+cfg = read_jsonfile(CONFIG_FILE)
 
 if not 'NOTIFY_CONTACTPAGER' in environ:
     complain('Environment variable NOTIFY_CONTACTPAGER missing')
 else:
     phone_number = environ['NOTIFY_CONTACTPAGER']
-    if re.match(PHONE_REGEX, phone_number):
+    if re.match(cfg['phone_regex'], phone_number):
         eagle_message = format_message()
         send_eagle_sms(phone_number, eagle_message)
